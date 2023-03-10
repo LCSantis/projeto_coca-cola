e5d7c2b79daf57f996511853f31a65188489c21f
LCSantis <lucas.santis@outlook.com.br>
2023-03-09 21:11:00 -0300

    Start form

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const menu = document.querySelector("#menu_lines");
+const menu = document.querySelector("#menu");
 const list = document.querySelector(".hidden_list")
 
 menu.addEventListener("mouseover", () => {
